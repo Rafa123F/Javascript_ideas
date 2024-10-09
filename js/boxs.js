2050c51a0e071924f331c7ef8a3bd7e7285e41e4
Rafa123F <rm.af@hotmail.com>
2024-10-09 17:14:24 +0400

    third commit

--- js/boxs.js
+++ js/boxs.js
@@ -77,20 +77,3 @@ header.addEventListener("mouseout", function () {
 ////////
 /////
 
-// select hero_module element
-const hero = document.querySelector('.hero__module');
-
-// I created function that runs a event listener to remove the last child element in the hero module:
-function removeElementOnce() {
-  hero.lastElementChild.remove();
-}
-//I registered the event listener on the document:
-document.addEventListener('click', removeElementOnce);
-//I tested my code. It works!!! But I'm not done yet because the listener is still active after the first click.
-
-//I added removeEventListener my event listener function to remove the listener after it has been triggered the first time:
-
-function removeElementOnce() {
-  hero.lastElementChild.remove();
-  document.removeEventListener('click', removeElementOnce);
-}
